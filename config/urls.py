15d06be64402f54fc2f8e55d3b5bfa06ee952124
Dave Caraway <dave@fogmine.com>
2015-08-02 10:16:47 -0400

    [FIXES ##100345918] Converted the account-close page to mezzanine

--- config/urls.py
+++ config/urls.py
@@ -83,6 +83,7 @@ urlpatterns += patterns('',
     url("^signup\.html$", direct_to_template, {"template": "signup.html"}, name="signup"),
     url("^forgot-password\.html$", direct_to_template, {"template": "forgot-password.html"}, name="forgot-password"),
     url("^account-home\.html$", direct_to_template, {"template": "account-home.html"}, name="account-home"),
+    url("^account-close\.html$", direct_to_template, {"template": "account-close.html"}, name="account-close"),
 
     #TODO should post-ads be deleted?
     url("^post-ads\.html$", direct_to_template, {"template": "post-ads.html"}, name="post-ads"),
